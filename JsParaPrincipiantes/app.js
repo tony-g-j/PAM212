c2981f0797b74dfe9702088f9d64ea7da2f48a68
TG <153114263+Xolaany@users.noreply.github.com>
2025-09-09 18:49:49 -0600

    Conociendo los tipos de datos JS

--- JsParaPrincipiantes/app.js
+++ JsParaPrincipiantes/app.js
@@ -1,1 +1,14 @@
 document.write("<h1>Hello World</h1>");
+//tipos de datos
+    "Hello world"//string
+    'Hello world'//string
+
+    100 //entero o int
+    15.5 //decimal o float
+
+    true //boleano
+    false //boleano
+
+    // array
+        ['joe', 'ryan', 'martha']
+        [1,2,3,4]
