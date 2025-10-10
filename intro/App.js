477a7f2760b6e0000aa0a9488559b472d4b4af1f
TG <153114263+Xolaany@users.noreply.github.com>
2025-10-09 18:24:43 -0600

    Uso de de componentes, propiedades y estados en un archivo Blank

--- intro/App.js
+++ intro/App.js
@@ -1,15 +1,24 @@
+// 1. import: Zona de declaraciones
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import {useState} from 'react'
 
+// 2. Main: Zona de componentes 
 export default function App() {
+  const[Contador, setContador] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Hola Mundo React Native!!</Text>
+      <Text>Contador: {Contador}</Text>
+      <Button title="Incrementar" onPress={()=>setContador(Contador + 1)}></Button>
+      <Button title="Quitar" onPress={()=>setContador(Contador - 1)}></Button>
+      <Button title="Reiniciar" onPress={()=>setContador(Contador - Contador)}></Button>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+// 3. Estilos: Zona de estetica y posicionamiento
 const styles = StyleSheet.create({
   container: {
     flex: 1,
